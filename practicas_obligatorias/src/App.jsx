70189981ec37fe8311607ed3f4fcedddaabe220a
AnaRustici <anabellarustici@gmail.com>
2024-04-28 22:23:34 -0300

    practica obligatoria 2

--- practicas_obligatorias/src/App.jsx
+++ practicas_obligatorias/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
+import Table from './components/table/Table'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Table></Table>
     </>
   )
 }
---
+++ practicas_obligatorias/src/components/table/Table.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Table.css';
+import { netIncomes } from '../netIncomes/NetIncomes'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Table = () => {
+    const mappedBrands = netIncomes.map((b, index) => (
+        <Col key={index}>{b.brand}</Col>
+    ))
+    const mappedIncomes = netIncomes.map((i, index) => (
+        <Col key={index}>{i.income}</Col>
+    ))
+    const sumIncomes = netIncomes.reduce(
+        (accumulator, currentValue) => accumulator + currentValue.income, 0
+    );
+    const average = sumIncomes / netIncomes.length
+
+    return (
+        <div>
+            <h2>Práctica obligatoria 2</h2>
+            <Container className='container'>
+                <Row>{mappedBrands}</Row>
+                <Row>{mappedIncomes}</Row>
+            </Container>
+            <p>Average incomes: {average.toFixed(2)}</p>
+        </div>
+
+    )
+}
+
+export default Table
